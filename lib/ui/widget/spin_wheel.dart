a4a969ffa83c0e3d65cdeef3d91bbed7c3f39be0
arisafrianto2304 <arisafrianto23@gmail.com>
2025-03-18 11:03:46 +0700

    lucky wheel

--- lib/ui/widget/spin_wheel.dart
+++ lib/ui/widget/spin_wheel.dart
@@ -98,140 +98,177 @@ class _SpinWheelState extends State<SpinWheel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(
-        255,
-        0,
-        30,
-        55,
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                      Colors.blue,
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const SpinWheel(),
-                      ),
-                    );
-                  },
-                  child: const Icon(
-                    Icons.refresh,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 100,
-                vertical: 20,
-              ),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(
-                  20,
-                ),
-              ),
-              child: Text(
-                rewards.toString(),
-                style: const TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Putaran ke: $count',
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(
-              height: 60,
-            ),
-            SizedBox(
-              height: 300,
-              child: FortuneWheel(
-                selected: selected.stream,
-                animateFirst: false,
-                items: [
-                  for (int i = 0; i < items.length; i++) ...<FortuneItem>{
-                    FortuneItem(
-                      child: Text(
-                        items[i].toString(),
+      // backgroundColor: const Color.fromARGB(255, 24, 19, 2),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/Kulit-Sintetis-MB-6001-Black.jpg'),
+            repeat: ImageRepeat.repeat, // tambahkan baris ini
+            fit: BoxFit.none,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10), // atur padding sesuai kebutuhan
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        const Color(0xFF997700),
                       ),
                     ),
-                  },
-                ],
-                onAnimationEnd: () {
-                  setState(() {
-                    rewards = items[selected.value];
-                    if (!['Putar Lagi', 'Coba Lagi'].contains(rewards)) {
-                      usedItems.add(rewards);
-                      items.removeAt(selected.value);
-                    }
-                  });
-                  if (!['Putar Lagi', 'Coba Lagi'].contains(rewards)) {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text('${rewards} Silahkan ke Depan !')));
-                  }
-                },
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                      Colors.blue,
-                    ),
-                  ),
-                  onPressed: () {
-                    if (items.isNotEmpty) {
-                      setState(() {
-                        count++;
-                        selected.add(Fortune.randomInt(0, items.length));
-                      });
-                    } else if (items.length == 2) {
-                      rewards = "Terima Kasih, Semua nama sudah muncul!";
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          content:
-                              Text('Terima Kasih, Semua nama sudah muncul!')));
-                    }
-                  },
-                  child: const Text(
-                    'PUTAR',
-                    style: TextStyle(
-                      fontSize: 40,
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SpinWheel(),
+                        ),
+                      );
+                    },
+                    child: const Icon(
+                      Icons.refresh,
                       color: Colors.white,
                     ),
                   ),
                 ),
-              ],
-            )
-          ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 100,
+                  vertical: 20,
+                ),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF997700),
+                  borderRadius: BorderRadius.circular(
+                    20,
+                  ),
+                ),
+                child: Text(
+                  rewards.toString(),
+                  style: const TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Putaran ke: $count',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              SizedBox(
+                height: 300,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    FortuneWheel(
+                      selected: selected.stream,
+                      animateFirst: false,
+                      indicators: <FortuneIndicator>[
+                        FortuneIndicator(
+                          alignment: Alignment
+                              .topCenter, // <-- changing the position of the indicator
+                          child: TriangleIndicator(
+                            color: const Color.fromARGB(255, 240, 240,
+                                240), // <-- changing the color of the indicator
+                          ),
+                        ),
+                      ],
+                      items: [
+                        for (int i = 0; i < items.length; i++)
+                          FortuneItem(
+                            child: Text(
+                              items[i].toString(),
+                            ),
+                            style: FortuneItemStyle(
+                              color: const Color(0xFF997700),
+                              borderColor:
+                                  const Color.fromARGB(255, 233, 195, 58),
+                              borderWidth: 3,
+                            ),
+                          ),
+                      ],
+                      onAnimationEnd: () {
+                        setState(() {
+                          rewards = items[selected.value];
+                          if (!['Putar Lagi', 'Coba Lagi'].contains(rewards)) {
+                            usedItems.add(rewards);
+                            items.removeAt(selected.value);
+                          }
+                        });
+                        if (!['Putar Lagi', 'Coba Lagi'].contains(rewards)) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                                content: Text('$rewards Silahkan ke Depan!')),
+                          );
+                        }
+                      },
+                    ),
+                    // Tambahkan gambar di sini
+                    Image.asset(
+                      'assets/logo-mbtech.png',
+                      width: 70,
+                      height: 70,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        const Color(0xFF997700),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (items.isNotEmpty) {
+                        setState(() {
+                          count++;
+                          selected.add(Fortune.randomInt(0, items.length));
+                        });
+                      } else if (items.length == 2) {
+                        rewards = "Terima Kasih, Semua nama sudah muncul!";
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                                    'Terima Kasih, Semua nama sudah muncul!')));
+                      }
+                    },
+                    child: const Text(
+                      'PUTAR',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
